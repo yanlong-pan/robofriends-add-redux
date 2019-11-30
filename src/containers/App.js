1003d10c846bc3982049c7c5fde6dc22042dbe95
pyl <a522723936@gmail.com>
2019-11-30 11:51:22 +1100

    code clean

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
-import './App.css';
-import {setSearchField, requestRobots} from '../action';
 
+import './App.css';
+
+// parameter state comes from index.js provider store state(rootReducers)
 const mapStateToProps = (state) => {
   return {
     searchField: state.searchRobots.searchField,
     robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending,
+    isPending: state.requestRobots.isPending
   }
 }
 
+// dispatch the DOM changes to call an action. note mapStateToProps returns object, mapDispatchToProps returns function
+// the function returns an object then uses connect to change the data from redecers.
 const mapDispatchToProps = (dispatch) => {
   return {
     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    // onRequestRobots: () => dispatch(requestRobots())
-    onRequestRobots: () => requestRobots()(dispatch)
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-
   componentDidMount() {
     this.props.onRequestRobots();
   }
 
   render() {
-    const { searchField, onSearchChange, robots, isPending } = this.props;
-    const filteredRobots = robots.filter(robot =>{
+    const { robots, searchField, onSearchChange, isPending } = this.props;
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
     return (
@@ -50,4 +53,5 @@ class App extends Component {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// action done from mapDispatchToProps will channge state from mapStateToProps
+export default connect(mapStateToProps, mapDispatchToProps)(App)
