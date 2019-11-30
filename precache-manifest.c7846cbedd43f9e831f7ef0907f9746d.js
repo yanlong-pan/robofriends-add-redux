dfe02bcba0a7039189fc508bee366a66c225758c
pyl <a522723936@gmail.com>
2019-11-30 13:25:19 +1100

    Updates

---
+++ precache-manifest.c7846cbedd43f9e831f7ef0907f9746d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4174e6ce0c44230d93a891b035c2dff",
+    "url": "/robofriends-add-redux/index.html"
+  },
+  {
+    "revision": "a1faeb88344ca5fa2583",
+    "url": "/robofriends-add-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "4f9bdc83d1c95ebf72e9",
+    "url": "/robofriends-add-redux/static/css/main.c8454ff2.chunk.css"
+  },
+  {
+    "revision": "a1faeb88344ca5fa2583",
+    "url": "/robofriends-add-redux/static/js/2.197e0458.chunk.js"
+  },
+  {
+    "revision": "4f9bdc83d1c95ebf72e9",
+    "url": "/robofriends-add-redux/static/js/main.ed0e5c19.chunk.js"
+  },
+  {
+    "revision": "dbe997c7ae7498e69c5d",
+    "url": "/robofriends-add-redux/static/js/runtime-main.cda1b4ae.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-add-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
